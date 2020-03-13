058ef158a36c93568662bb1d28b2c84ff5cf44a0
Mahib7009 <50058440+Mahib7009@users.noreply.github.com>
2020-03-13 18:44:28 -0400

    Add files via upload

---
+++ FINFO/calculator2.js
@@ -0,0 +1,25 @@
+﻿function calc2()
+{
+	var annual_salary = 1, portion_saved = 1, total_cost = 1, current_savings = 0, month = 0;
+	
+	annual_salary = document.getElementById("text1").value;
+	portion_saved = document.getElementById("text2").value;
+	total_cost = document.getElementById("text3").value;
+	current_savings = document.getElementById("text4").value;
+	if(portion_saved >= 1)
+	{
+		portion_saved = portion_saved/100;
+	}
+	
+	var down_payment = total_cost * 0.25;
+	var rate = (0.04 / 12) + 1;
+						
+	while (current_savings < down_payment)
+	{
+	current_savings = ((current_savings + (portion_saved * (annual_salary / 12))) * rate);
+	month += 1;
+	}
+						
+	document.getElementById("answer").innerHTML = month;
+}
+
---
+++ FINFO/calculator4.js
@@ -0,0 +1,19 @@
+﻿function calc4()
+{
+	var income, savings_rate, years, total_savings = 0;
+	
+	income = document.getElementById("text1").value;
+	savings_rate = document.getElementById("text2").value;
+	years = document.getElementById("text3").value;
+
+	savings_rate = savings_rate/100;
+					
+	for (var i = 0; i < years; i++)
+	{
+		total_savings += income*savings_rate;
+		total_savings = total_savings*1.07;
+	}
+
+						
+	document.getElementById("answer").innerHTML = total_savings;
+}
